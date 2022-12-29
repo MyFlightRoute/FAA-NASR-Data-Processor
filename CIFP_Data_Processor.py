2f480d176badb888363d29ff0de3ce7711f6dd22
Thomas Saunders <thomas@vesula.com>
2022-12-29 23:28:31 +0000

    Simplify datacheck function

--- CIFP_Data_Processor.py
+++ CIFP_Data_Processor.py
@@ -4,26 +4,10 @@ import wget
 import time
 
 
-def initialise_data(skip_folder=False):
+def initialise_data():
 
-    does_data_folder_exist = os.path.exists('data')
-    does_cifp_data_notice_exist = os.path.exists('data/CIFP_Data_Here.txt')
     does_cifp_faacifp18_file_exist = os.path.exists('data/FAACIFP18')
     does_cifp_in_cifp_file_exist = os.path.exists('data/IN_CIFP.txt')
-
-    if not skip_folder:
-        if not does_data_folder_exist:
-            os.mkdir('data')
-        else:
-            print("Data folder exists")
-
-    if not does_cifp_data_notice_exist:
-        print('CIFP Data Notice is missing, downloading')
-        wget.download("https://raw.githubusercontent.com/MyFlightRoute/CIFP-Data-Processor/dev/data/CIFP_Data_Here.txt", 'data/CIFP_Data_Here.txt')
-
-        initialise_data(True)
-    else:
-        print('CIFP Data Notice exists')
 
     if not does_cifp_faacifp18_file_exist:
         print("CIFP FAACIFP18 file missing")
