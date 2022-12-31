aa7936b31643e7d6a3a28992e0e4e87447c5021d
Thomas Saunders <thomas@vesula.com>
2022-12-31 22:35:44 +0000

    Add progress bar constants

--- CIFP-Data-Processor/Globals.cs
+++ CIFP-Data-Processor/Globals.cs
@@ -1,7 +1,15 @@
+using ShellProgressBar;
+
 namespace CIFP_Data_Processor
 {
     public static class Globals
     {
-        public static string[] RAW_DATA;
+        public static string[] RawData;
+
+        public static readonly ProgressBarOptions ProgressBarOptions = new ProgressBarOptions()
+        {
+            ProgressCharacter = '-',
+            ProgressBarOnBottom = true
+        };
     }
 }
