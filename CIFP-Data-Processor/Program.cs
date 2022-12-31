c5675462a0f7ac375ab631745916a3c8fe390e8f
Thomas Saunders <thomas@vesula.com>
2022-12-31 22:48:16 +0000

    Add function to read data in

--- CIFP-Data-Processor/Program.cs
+++ CIFP-Data-Processor/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using ConsoleMenu;
+using System.IO;
+using System.Threading;
 
 namespace CIFP_Data_Processor
 {
@@ -7,9 +9,38 @@ namespace CIFP_Data_Processor
     {
         static void Main(string[] args)
         {
-            mainMenu();
+            if (!Directory.Exists("data"))
+            {
+                Directory.CreateDirectory("data");
+            }
+
+            while (true)
+            {
+                Console.Clear();
+                mainMenu();
+            }
         }
 
+        static string[] ReadCifpData()
+        {
+            if (!File.Exists("data/FAACIFP18"))
+            {
+                Console.WriteLine("Please download the FAA CIFP Data and try again.");
+                
+                Thread.Sleep(2000);
+
+                return null;
+            }
+
+            string[] data = File.ReadAllLines("data/FAACIFP18");
+            
+            Console.WriteLine("Data read in.");
+            
+            Thread.Sleep(2000);
+            
+            return data;
+        }
+        
         static void mainMenu()
         {
             string[] menuItems = new[] { "Read data", "Export airports", "Exit"};
@@ -18,13 +49,15 @@ namespace CIFP_Data_Processor
 
             switch (menu.displayMenu())
             {
+                case 0:
+                    Globals.RawData = ReadCifpData();
+                    break;
+                
                 case 1:
-                    // Todo: Develop reading data in
+                    // Todo: Develop exporting airport
+                    break;
                 
                 case 2:
-                    // Todo: Develop exporting airport
-                
-                case 3:
                     Environment.Exit(0);
                     break;
                 
