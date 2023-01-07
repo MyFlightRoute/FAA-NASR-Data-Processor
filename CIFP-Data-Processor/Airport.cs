640e888144d0e63d9e2cab4b7ca6791f9bdd5a76
Thomas Saunders <thomas@vesula.com>
2023-01-07 23:50:05 +0000

    Add fields for FAA Code and ICAO Code

--- CIFP-Data-Processor/Airport.cs
+++ CIFP-Data-Processor/Airport.cs
@@ -5,7 +5,8 @@ namespace CIFP_Data_Processor
     public class Airport
     {
         public String Name { get; set; }
-        public String Identifier { get; set; }
+        public String IcaoCode { get; set; }
+        public String FaaCode { get; set; }
         public String Latitude { get; set; }
         public String Longitude { get; set; }
         
