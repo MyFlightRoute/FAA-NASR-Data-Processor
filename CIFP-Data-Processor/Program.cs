4997a3977a1257114bb2f8e49462fd975907e8cd
Thomas Saunders <thomas@vesula.com>
2023-01-07 23:56:24 +0000

    Add progress bar to airport processing

--- CIFP-Data-Processor/Program.cs
+++ CIFP-Data-Processor/Program.cs
@@ -45,33 +45,43 @@ namespace CIFP_Data_Processor
 
             Console.WriteLine("Raw airport data read in successfully. {0} records added.", airportHoldingData.Count.ToString());
             
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
 
-            for (int i = 0; i < airportHoldingData.Count; i++)
+            using (ProgressBar progressBar = new ProgressBar(airportHoldingData.Count, "Processing airport data",
+                       Globals.ProgressBarOptions))
             {
-                Airport newAirport = new Airport();
-                char[] dataCurrentLineChar = airportHoldingData[i].ToCharArray();
-                bool duplicateAirport = false;
-                string airportName, faaCode, icaoCode, latitude, longitude, lastIcaoCode;
-
-                icaoCode = generateIcaoCode(dataCurrentLineChar);
-
-                if (i > 0)
+                for (int i = 0; i < airportHoldingData.Count; i++)
                 {
-                    char[] dataLastLineChar = airportHoldingData[i - 1].ToCharArray();
-                    lastIcaoCode = generateIcaoCode(dataLastLineChar);
+                    Airport newAirport = new Airport();
+                    char[] dataCurrentLineChar = airportHoldingData[i].ToCharArray();
+                    bool duplicateAirport = false;
+                    string airportName, faaCode, icaoCode, latitude, longitude, lastIcaoCode;
 
-                    duplicateAirport = (icaoCode == lastIcaoCode);
-                }
-                
-                newAirport.IcaoCode = icaoCode;
+                    icaoCode = generateIcaoCode(dataCurrentLineChar);
 
-                if (!duplicateAirport)
-                {
-                    airports.Add(newAirport);
+                    if (i > 0)
+                    {
+                        char[] dataLastLineChar = airportHoldingData[i - 1].ToCharArray();
+                        lastIcaoCode = generateIcaoCode(dataLastLineChar);
+
+                        duplicateAirport = (icaoCode == lastIcaoCode);
+                    }
+
+                    newAirport.IcaoCode = icaoCode;
+
+                    if (!duplicateAirport)
+                    {
+                        airports.Add(newAirport);
+                    }
+                    
+                    progressBar.Tick();
                 }
             }
 
+            Console.WriteLine("Airports list generated. There are {0} airports in the database.", airports.Count);
+            
+            Thread.Sleep(2000);
+            
             return airports;
         }
 
@@ -83,13 +93,13 @@ namespace CIFP_Data_Processor
             {
                 // Three letter code
                 icaoCode = rawData[6].ToString() + rawData[7].ToString() + rawData[8].ToString();
-                Console.WriteLine(icaoCode);
+                // Console.WriteLine(icaoCode);
             }
             else if (rawData[10] == 'K')
             {
                 // Four letter code
                 icaoCode = rawData[6].ToString() + rawData[7].ToString() + rawData[8].ToString() + rawData[9].ToString();
-                Console.WriteLine(icaoCode);
+                // Console.WriteLine(icaoCode);
             }
             else
             {
