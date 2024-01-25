d0f3f84b05296fa1819348d73af8d0f6d23daa28
Thomas Saunders <thomas@aviationsourcenews.com>
2024-01-25 17:59:44 +0000

    Basic application information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
 fn main() {
+    const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
     fs::create_dir_all("./data").expect("Failed to create");
+    
+    println!("FAA NASR Data Processor v{}", VERSION.unwrap_or("Unknown"));
 }
