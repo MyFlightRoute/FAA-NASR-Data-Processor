63d3060786e3de7d266475d88ad2c01bad20c139
Thomas Saunders <thomas@aviationsourcenews.com>
2024-01-30 13:10:09 +0000

    Remove old processing code.

--- src/modified_airport.rs
+++ src/modified_airport.rs
@@ -10,12 +10,14 @@ pub struct ModifiedAirport {
 }
 
 impl ModifiedAirport {
-    pub fn run_changes(&mut self) {
+    // Below function is no longer required due to revised processing of finding airport changes in main.rs.
+    // Leaving commented for future reference, in case this processing method becomes necessary.
+    /* pub fn run_changes(&mut self) {
         self.is_modified.replace(!(self.current_airport.as_mut().unwrap() != self.new_airport.as_mut().unwrap()));
 
         self.renamed.replace(!(self.current_airport.as_mut().unwrap().airport_name == self.new_airport.as_mut().unwrap().airport_name));
 
         self.closed.replace(self.new_airport.is_none());
         self.opened.replace(self.current_airport.is_none());
-    }
+    } */
 }
