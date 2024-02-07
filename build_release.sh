59431acc0f4977f348070ece849b36c4f7d49f5f
Thomas Saunders <thomas@aviationsourcenews.com>
2024-02-07 15:42:17 +0000

    Change WIndows to GNU

--- build_release.sh
+++ build_release.sh
@@ -1,9 +1,9 @@
 rustup target add x86_64-unknown-linux-musl
-rustup target add x86_64-pc-windows-msvc
+rustup target add x86_64-pc-windows-gnu
 rustup target add x86_64-apple-darwin
 rustup target add aarch64-apple-darwin
 
 cross build --release --target x86_64-unknown-linux-musl
-cross build --release --target x86_64-pc-windows-msvc
+cross build --release --target x86_64-pc-windows-gnu
 cross build --release --target x86_64-apple-darwin
 cross build --release --target aarch64-apple-darwin
