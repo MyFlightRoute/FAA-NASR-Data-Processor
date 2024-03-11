7168c818ed2b2c76c024b6c0266b53d47356039c
Thomas Saunders <thomas@aviationsourcenews.com>
2024-03-11 11:03:15 +0000

    TEC Route struct

---
+++ src/tec_route.rs
@@ -0,0 +1,23 @@
+pub struct TecRoute {
+    origin_id: String,
+    origin_city: String,
+    origin_state_code: String,
+    origin_country_code: String,
+    destination_id: String,
+    destination_city: String,
+    destination_state_code: String,
+    destination_country_code: String,
+    pfr_type_code: String,
+    route_number: String,
+    special_area_description: String,
+    altitude_description: String,
+    aircraft: String,
+    hours: String,
+    route_dir_description: String,
+    designator: String,
+    nar_type: String,
+    inland_fac_fix: String,
+    coastal_fix: String,
+    destination: String,
+    route_string: String
+}
