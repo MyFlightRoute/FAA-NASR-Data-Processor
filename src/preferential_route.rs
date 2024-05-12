605f5165c0f888c7ad4c9b2794efd004cf87ad74
Thomas Saunders <thomas@aviationsourcenews.com>
2024-05-12 12:47:11 +0100

    Handle setting blank route string to DCT on REC Route being read

--- src/preferential_route.rs
+++ src/preferential_route.rs
@@ -109,6 +109,10 @@ fn read_tec_routes(future_data: bool) -> Vec<PreferentialRoute> {
                     route_notes: None
                 };
 
+                if new_tec_route.route_string == "" {
+                    new_tec_route.route_string = String::from("DCT");
+                }
+                
                 if new_tec_route.special_area_description.contains("LAXE") {
                     new_tec_route.route_notes = Option::from(String::from("LAX EAST"));
                 } else if new_tec_route.special_area_description.contains("LAXW") {
@@ -187,16 +191,6 @@ pub fn generate_tec_route_changes() {
             let altitude_change: bool = current_route_loop.altitude_description != new_route.as_ref().unwrap().altitude_description;
             let route_change: bool = current_route_loop.route_string != new_route.as_ref().unwrap().route_string;
             let mut new_route_modifier: Option<PreferentialRoute>;
-
-
-            if new_route.unwrap().route_string == "" {
-                new_route_modifier = new_route.cloned();
-
-                new_route_modifier.as_mut().unwrap().route_string = "DCT".to_string();
-
-                new_route = new_route_modifier.as_ref();
-
-            }
 
             if route_change || altitude_change {
                 let modified_route: ModifiedRoute = ModifiedRoute {
