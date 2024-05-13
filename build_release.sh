82c49269843a4582966ec9eafb9974bfc5ed020b
Thomas Saunders <thomas@aviationsourcenews.com>
2024-05-13 11:19:01 +0100

    Automatically copy and rename builds for upload

--- build_release.sh
+++ build_release.sh
@@ -7,3 +7,15 @@ cross build --release --target x86_64-unknown-linux-musl
 cross build --release --target x86_64-pc-windows-gnu
 cross build --release --target x86_64-apple-darwin
 cross build --release --target aarch64-apple-darwin
+
+cd target/x86_64-apple-darwin/release
+cp faa-nasr-data-processor ../../release/faa-nasr-data-processor_x86_64-apple-darwin
+
+cd ../../x86_64-unknown-linux-musl/release
+cp faa-nasr-data-processor ../../release/faa-nasr-data-processor_x86_64-unknown-linux-musl
+
+cd ../../x86_64-pc-windows-gnu/release
+cp faa-nasr-data-processor.exe ../../release/faa-nasr-data-processor_x86_64-windows-gnu.exe
+
+cd ../../aarch64-apple-darwin/release
+cp faa-nasr-data-processor ../../release/faa-nasr-data-processor_aarch64-apple-darwin
