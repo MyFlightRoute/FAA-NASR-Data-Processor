087130b837e4143c07e60b00d5f2c06b859cc001
Thomas Saunders <psn.github@ts379.co.uk>
2024-05-23 20:40:40 +0100

    Create output folder for processed files

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ const ONE_SECOND:Duration = Duration::from_secs(1);
 const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
 
 fn main() {
-    fs::create_dir_all("./data").expect("Failed to create");
+    fs::create_dir_all("./data").expect("Failed to create data folder");
+    fs::create_dir_all(("./data/output")).expect("Failed to create output folder");
 
     main_menu();
 }
